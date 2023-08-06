83357db57853ed37c08ec00a0283a9d5ccc6e9ad
Enes Ergen <eenes44@gmail.com>
2023-08-06 18:02:41 +0300

    Completed

--- src/main/java/ProcessUnit.java
+++ src/main/java/ProcessUnit.java
@@ -18,6 +18,8 @@ import java.net.Socket;
 public class ProcessUnit {
     private static int port;
 
+
+
     public void createServer(int port) {
         try (ServerSocket server = new ServerSocket(port)) {
             server.setReuseAddress(true);
@@ -42,7 +44,46 @@ public class ProcessUnit {
             this.sensor = socket;
         }
 
+        public String[] stringToDataArray(String data){
+            String[]dataArray=data.split(",");
+            /*
+            * index - value
+            * 0 sensorName
+            * 1 sensorX
+            * 2 sensorY
+            * 3 sensorXVelocity
+            * 4 sensorYVelocity
+            * 5 targetName
+            * 6 targetX
+            * 7 targetY
+            * 8 targetXVelocity
+            * 9 targetYVelocity
+            * */
+            return dataArray;
+        }
+        public void processUnit(String []data){
+            if(data.length==10){
+                String msg="";
+                double difX=Double.parseDouble(data[6])-Double.parseDouble(data[1]);
+                double difY=Double.parseDouble(data[7])-Double.parseDouble(data[2]);
+                double difXVelocity=Double.parseDouble(data[8])-Double.parseDouble(data[3]);
+                double difYVelocity=Double.parseDouble(data[9])-Double.parseDouble(data[4]);
+                if(difX==difY && difY==0){
+                    msg+="Hedef ve sensor aynı noktada,";
+                }else{
+                    double angle=(Math.atan2(difX, difY) * (180 / Math.PI));
+                    if(angle<=0){
+                        angle+=360;
+                    }
+                    msg+=data[0]+" için "+data[5]+" kerterizi Y pozitif saat yönünde açısı:"+angle+" derecedir.";
+                    msg+=data[0]+" için "+data[5]+" bağıl hızı X ekseni için:"+difXVelocity+",Y ekseni için:"+difYVelocity+" değeridir.";
+                    System.out.println(msg);
+                }
+            }else{
+                System.out.println("data is wrong");
+            }
 
+        }
         @Override
         public void run() {
             try (
@@ -51,8 +92,13 @@ public class ProcessUnit {
             ) {
                 while (true) {
                     String data = (String) ois.readObject();
-                    oos.writeObject("Process Unit:Data received");
-                    System.out.println(data);
+                    if (data == "") {
+                        oos.writeObject("Process Unit:NULL Data received");
+                    } else {
+                        oos.writeObject("Process Unit:Data received");
+                        String[]dataArr=stringToDataArray(data);
+                        processUnit(dataArr);
+                    }
                 }
 
             } catch (IOException | ClassNotFoundException e) {
@@ -85,9 +131,9 @@ public class ProcessUnit {
     }
 
     public static void main(String[] args) {
-        readObjectFromXML("C:\\Users\\stj.eergen\\Desktop\\Processor\\src\\main\\resources\\ProcessUnit.xml");
-        ProcessUnit processUnit=new ProcessUnit();
-        new Thread(()->{
+        readObjectFromXML("src/main/resources/ProcessUnit.xml");
+        ProcessUnit processUnit = new ProcessUnit();
+        new Thread(() -> {
             processUnit.createServer(port);
         }).start();
     }
